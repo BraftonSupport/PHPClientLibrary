5d5cedb0d8324e0a5b3953da1678427d9770b60a
dking3876 <dking3876@msn.com>
2016-04-19 15:30:42 -0400

    add commenting to the connection class

--- VideoAPILibrary/AdferoConnection.php
+++ VideoAPILibrary/AdferoConnection.php
@@ -1,4 +1,5 @@
 <?php
+//Used to determine type of connection to use in retrieving the XML from the api
 class AdferoConnection{
     
     public static $instance = null;
@@ -15,22 +16,25 @@ class AdferoConnection{
     public function __construct($url){
         $this->url = $url;
         $this->get_connection_type();
-        var_dump($this->connection_type);
         
     }
-    
+    //Determine the connection type.  If a connection choice was passed to any of the 3 main Clients (Articles, Video, Photo) that connection will be set.
     private function get_connection_type(){
+        //Check if the connection has been hard set by passing a connection to 1 of the 3 main clients.  If it has use that connection
         if(self::$force_connection){
             $this->connection_type = self::$force_connection;
             return;
         }
+        //Look for fopen and allow_url_fopen settings to allow a simple connection
         if(function_exists('fopen') && ini_get('allow_url_fopen')){
             $this->connection_type = 'fopen';
         }
+        //Check if curl is available if the above option was not available
         else if(function_exists('curl_init')){
             $this->connection_type = 'curl';
         }
     }
+    //Retrieve the xml as a string
     public function getFeed(){
         if($this->connection_type != null){
             $connect = $this->connection_type.'_connection';
@@ -40,6 +44,7 @@ class AdferoConnection{
         }
         return false;
     }
+    //Retrieve the XML using curl
     private function curl_connection(){
         $url = $this->url;
         if(!isset($ch)){
@@ -51,17 +56,17 @@ class AdferoConnection{
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $feed_string = curl_exec($ch); 
         $hinfo = curl_getinfo($ch);
-        $this->parseHeaders_curl($hinfo);
+        $this->parse_headers_curl($hinfo);
         return $feed_string;
     }
-    
+    //Retrieve the XML using fopen
     private function fopen_connection(){
         $con = file_get_contents($this->url);
-        $this->parseHeaders_fopen($http_response_header);
+        $this->parse_headers_fopen($http_response_header);
         return $con;
     }
-    
-    private function parseHeaders_fopen( $headers ){
+    //Parse the headers for fopen
+    private function parse_headers_fopen( $headers ){
         $head = array();
         foreach( $headers as $k=>$v )
         {
@@ -79,11 +84,13 @@ class AdferoConnection{
 
         $this->response = $head;
     }
-    private function parseHeaders_curl($headers){
+    //Parse headers for curl
+    private function parse_headers_curl($headers){
         $headers['Content_type'] = explode(';', $headers['content_type']);
 
         $this->response = $headers;
     }
+    //Set the forced connection type 
     public static function force_connection($connection){
         if(self::$force_connection){
             return;   
